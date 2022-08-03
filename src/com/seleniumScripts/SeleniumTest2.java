e3156c304b6827a9cd29d5a4b119aa902fd9d062
celsiusdv <celsius.dev@protonmail.com>
2022-08-03 03:06:17 -0400

    getting blob links andthumbnails links from playlist panel on the right side

--- src/com/seleniumScripts/SeleniumTest2.java
+++ src/com/seleniumScripts/SeleniumTest2.java
@@ -1,9 +1,6 @@
 package com.seleniumScripts;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,11 +13,10 @@ public class SeleniumTest2 {
     WebDriver driver;
     JavascriptExecutor js;
     WebElement playlist,player;
-    List<WebElement> videos;
+    List<WebElement> videos,playerControls;
     public SeleniumTest2() {
         System.setProperty("webdriver.chrome.driver","src/chromedriver");
         ChromeOptions options = new ChromeOptions();
-
         options.setBinary("/usr/bin/brave-browser");
         options.addArguments("--start-maximized");
         options.addArguments("--disable-extensions");
@@ -33,9 +29,9 @@ public class SeleniumTest2 {
         System.out.println(driver.getTitle());
         try{
             this.enteringPlaylist();
-            this.extractThumbnail();
-        }catch (Exception e){
-            System.out.println(e.getCause());
+            this.extractTitleAndSource();
+        }catch (InterruptedException | WebDriverException e){
+            e.getCause();
         }finally {
             this.closeDriverandBrowser();
             System.out.println("drivers and browser closed successfully");
@@ -49,16 +45,44 @@ public class SeleniumTest2 {
         System.out.println("clicking on first element");
         videos.get(0).findElement(By.id("video-title")).click();
     }
-    public void extractThumbnail(){
+    public void extractThumbnails(int index){//2º extracting thumbnails from the playlist
         WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(15));
         wait.until(ExpectedConditions.visibilityOf(player=driver.findElement(By.tagName("video") ) ) );
         System.out.println("video player loaded");
+
         playlist=driver.findElement(By.id("items"));
         videos=playlist.findElements(By.xpath("//*[@id=\"playlist-items\"]"));
-        System.out.println("videos items list size: "+videos.size());
 
-        System.out.println(videos.get(0).findElement(By.tagName("img")).getAttribute("src"));
+        System.out.println("videos items list size: "+videos.size());
+        System.out.println("thumbnail link: "+videos.get(index).findElement(By.tagName("img")).getAttribute("src"));
     }
+
+    public void extractTitleAndSource() throws InterruptedException, WebDriverException {//3º getting sources from video
+        int index=0;
+            Thread sourceVideo=new Thread(() -> {
+                try {
+                    Thread.sleep(400);
+                    this.extractThumbnails(index);
+                    System.out.println("blob link= "+player.getAttribute("src"));
+
+                } catch (InterruptedException e) {
+                    System.out.println("error in sourceVideo thread: no elements found");
+                    throw new RuntimeException(e);
+                }
+            });
+            sourceVideo.start();
+            sourceVideo.join();
+            Thread clickNext=new Thread(() -> {//get title and click in the second element (next button) from playerControls list
+                System.out.println(driver.getTitle());
+                player=driver.findElement(By.className("ytp-left-controls"));
+                playerControls=player.findElements(By.tagName("a"));//all controls (previous,play, next buttons)are loaded in playerControl list
+                System.out.println("clicking next element");
+                playerControls.get(1).click();
+            });
+            clickNext.start();
+            clickNext.join();
+    }
+
     public void closeDriverandBrowser(){
         try {Thread.sleep(100);} catch (InterruptedException e) {System.out.println("error from sleep thread");}
         System.out.println("\nclosing browser");
